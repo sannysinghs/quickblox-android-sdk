fd2b5db0e9162441f461a597021ed4f1f432d006
Roman Pronin <roman.pronin@injoit.com>
2017-10-12 20:02:46 +0300

    add createChannel for 26 API

--- sample-core/src/main/java/com/quickblox/sample/core/gcm/CoreGcmPushListenerService.java
+++ sample-core/src/main/java/com/quickblox/sample/core/gcm/CoreGcmPushListenerService.java
@@ -1,9 +1,12 @@
 package com.quickblox.sample.core.gcm;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.DrawableRes;
 import android.util.Log;
 
 import com.quickblox.messages.services.gcm.QBGcmPushListenerService;
+import com.quickblox.sample.core.R;
 import com.quickblox.sample.core.utils.ActivityLifecycle;
 
 public abstract class CoreGcmPushListenerService extends QBGcmPushListenerService {
@@ -18,6 +21,17 @@ public abstract class CoreGcmPushListenerService extends QBGcmPushListenerServic
         if (ActivityLifecycle.getInstance().isBackground()) {
             showNotification(message);
         }
+    }
+
+    @DrawableRes
+    protected int getNotificationIcon() {
+        int icon;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            icon = R.mipmap.ic_notification;
+        } else {
+            icon = R.mipmap.ic_launcher;
+        }
+        return icon;
     }
 
     protected abstract void showNotification(String message);
